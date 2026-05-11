9f19144ae35a2d9f10c76e639ef17c56c1d0858c
belajarcodinx-hub <belajarcodinx@gmail.com>
2026-05-11 12:10:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let user =  prompt('masukan user anda')
+let password =  prompt('masukan password anda')
+let user = rival1989
+let password = kmzwa8awaa
+
+// cara if else//
+if (user == 'rival1989') {
+    user = 'anda berhasil log in'
+} else {
+    user = 'user anda salah'
+}
+
+if (password == 'kmzwa8awaa') {
+    password = 'pssword anda benar'
+} else {
+    password = 'password anda salah'
+}
+
+console.log('user anda adalah ' + user + ' dan password anda adalah ' + password )
+
+
+
+
+
+
+
+
+
+// // cara switch case //
+// switch (status) {
+//     case null:
+//         status = 'sabar ya anda masih karyawan kontrak'
+//         break
+//     case 1:
+//         status = 'alhamdulilah anda sudah pegawai tetap ya'
+//         break
+//     default:
+//         status = ' ah didinyamah lain pegawai damri'
+//         break
+// }
